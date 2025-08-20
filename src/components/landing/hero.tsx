0917c7c25b353694351bdb761d05b4a549f31257
Shoaib Badshah <shoaibbadshah2@gmail.com>
2025-08-20 07:10:09 +0000

    also apply lg:[122px]

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -70,7 +70,7 @@ export const Hero = ({
             loop 
             muted 
             playsInline
-            className="absolute z-0 w-auto min-w-full min-h-full max-w-none pb-[122px]"
+            className="absolute z-0 w-auto min-w-full min-h-full max-w-none lg:pb-[122px]"
         >
             <source src="/assets/images/hero/hadley-heights1.mp4" type="video/mp4" />
             Your browser does not support the video tag.
